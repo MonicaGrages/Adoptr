342febb77c4e538f9cbc4323f12f76e6616f7e70
MonicaGrages <mgrages1@gmail.com>
2017-07-09 19:08:21 -0400

    adding header component

---
+++ client/components/header/header.controller.js
@@ -0,0 +1,9 @@
+HeaderController.$inject = ['SearchService'];
+function HeaderController(SearchService){
+  var vm = this;
+  activate();
+
+  function activate(){
+  }
+}
+export default HeaderController;
