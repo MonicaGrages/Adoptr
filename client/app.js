6bfc00db69111034a0c623c890957a9cb507e878
MonicaGrages <mgrages1@gmail.com>
2017-07-10 11:40:43 -0400

    removed debugging console logs

--- client/app.js
+++ client/app.js
@@ -14,7 +14,6 @@ angular.module("AdoptrApp", ["ui.router", "ipCookie", "ng-token-auth"])
 
 router.$inject = ["$stateProvider", "$urlRouterProvider"];
 function router ($stateProvider, $urlRouterProvider) {
-  console.log("In the Router!")
   $stateProvider
     .state("petSearch", {
       url: "/",
--- client/components/pets/search/search.controller.js
+++ client/components/pets/search/search.controller.js
@@ -5,7 +5,6 @@ function SearchController(SearchService, $auth, $state){
   activate();
 
   function activate(){
-    console.log("in search controller")
     SearchService.getPet()
     .then(response => {
       vm.pet = response;
