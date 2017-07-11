869dc2d1399a51655e02cbe28e1ce9292d083a27
MonicaGrages <mgrages1@gmail.com>
2017-07-11 15:40:57 -0400

    cleaning up comments

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,7 +6,6 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
     render json: @pet,
-      # include: [{:favorites => {:only => [:id, :user_id, :created_at]}}, {:contact => {:only [:phone, :email, :city, :state, :zip]}}],
       include: [:favorites, :contact]
   end
 end
