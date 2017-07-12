a097825d46c1563f6163433ca39b882b3732417a
MonicaGrages <mgrages1@gmail.com>
2017-07-12 16:44:40 -0400

    trying to get action mailer to work on heroku. need to set up sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # https://devcenter.heroku.com/articles/sendgrid
   config.action_mailer.default_url_options = { :host => 'adoptr.herokuapp.com' }
 
 
