afcc47fc4e8662b062c12b524c4ac9fbf4968191
MonicaGrages <mgrages1@gmail.com>
2017-07-12 15:51:08 -0400

    added password reset link. it doesn't work but gives user an error message

--- client/components/users/user_sessions/user_sessions.controller.js
+++ client/components/users/user_sessions/user_sessions.controller.js
@@ -2,6 +2,7 @@ UserSessionsController.$inject = ['$auth', '$state'];
 
 function UserSessionsController($auth, $state){
   var vm = this;
+  vm.passwordResetForm = {};
   activate();
 
   function activate(){
@@ -22,6 +23,32 @@ function UserSessionsController($auth, $state){
         vm.loginErrorMessage = "Email/Password combination not found. Try again.";
       });
   };
+
+  vm.passwordReset = function() {
+    console.log('reset');
+    $auth.requestPasswordReset(vm.passwordResetForm)
+        .then(function(response) {
+          // handle success response
+          console.log('success');
+          console.log(response);
+          // vm.passwordResetResponse = "Password reset email sent to vm."
+        })
+        .catch(function(response) {
+          vm.passwordResetResponseMessage = response.data.errors[0];
+          // handle error response
+          console.log('error');
+          console.log(response);
+        });
+    // $auth.requestPasswordReset(passwordResetForm)
+    //     .then(function(response) {
+    //       // handle success response
+    //       console.log(response);
+    //     })
+    //     .catch(function(response) {
+    //       // handle error response
+    //       console.log(response);
+    //     });
+  }
 
 
 }
